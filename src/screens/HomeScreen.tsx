45ea5a4730ed786465788fb153a6d5f9446b97d7
Gabriel Gomes <gabriel@mpsouza.com.br>
2025-06-27 17:10:24 -0300

    1º navegação e tela Home

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo à Lista de Compras!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
